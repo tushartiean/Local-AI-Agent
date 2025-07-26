960312b583bd856993c6abcf72bac4d52d3eb156
tushartiean <tushartiean7@gmail.com>
2025-07-26 21:34:33 +0530

    Add files via upload

---
+++ vector.py
@@ -0,0 +1,37 @@
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+import os
+import pandas as pd
+
+df = pd.read_csv("nike_data_13-09-2023.csv")
+embeddings = OllamaEmbeddings(model="mxbai-embed-large")
+
+db_location = "./chrome_langchain_db"
+add_documents = not os.path.exists(db_location)
+
+if add_documents:
+    documents = []
+    ids = []
+    
+    for i, row in df.iterrows():
+        document = Document(
+            page_content=row["title"] + " " + row["Review"],
+            metadata={"Rating": row["Rating"], "Rate for two": row["Rate for two"]},
+            id=str(i)
+        )
+        ids.append(str(i))
+        documents.append(document)
+        
+vector_store = Chroma(
+    collection_name="restaurant_reviews",
+    persist_directory=db_location,
+    embedding_function=embeddings
+)
+
+if add_documents:
+    vector_store.add_documents(documents=documents, ids=ids)
+    
+retriever = vector_store.as_retriever(
+    search_kwargs={"k": 5}
+)
